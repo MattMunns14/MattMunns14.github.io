8206ce120941f742aa30e7996f18148e4eb0c7c2
Matt Munns <matthewmunns0@gmail.com>
2019-07-19 17:46:35 -0400

    More about changes

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App(){
                               </p>
                               <p>
                                 I'm on some websites. Here's my <a href ="https://www.linkedin.com/in/matthew-munns-98727b138">Linkedin</a>. 
-                                Here's my <a href="https://github.com/MattMunns14">Github</a>. I also have a <a href="https://mattmunns14.github.io/GMM_Report.pdf">resume</a>. 
+                                Here's my <a href="https://github.com/MattMunns14">Github</a>. I also have a <a href="https://mattmunns14.github.io/Resume.pdf">resume</a>. 
                               </p>
                              </div>
      </TabContainer>}
